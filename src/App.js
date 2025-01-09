17545a2976eae49d463df8d7a5d1d34e7ba61fb5
Saksham Gera <sakshamgera1456@gmail.com>
2025-01-10 00:07:43 +0530

    Map Integrated

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Admin from 'Admin';
 import HomePage from 'pages/HomePage';
+import FloatingMap from 'components/FloatingMap';
 
 function App() {
   return (
-    <Router>
+    <Router >
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/dashboard" component={Admin} />
           <Route component={HomePage} /> 
         </Switch>
+        <FloatingMap />
       </div>
     </Router>
   );
